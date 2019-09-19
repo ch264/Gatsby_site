deb87b936c25e3e0e3f74bf5e5c128e6f7a1bf88
ch264 <christinavhastenrath@gmail.com>
2019-09-19 11:22:17 -0700

    deleted folder

--- markdownlint.js
+++ markdownlint.js
@@ -5,7 +5,7 @@ console.log("contents", contents);
 const markdownlint = require("markdownlint");
 
 const options = {
-  "files": [ "ga-site/test2.md", "test.md" ],
+  "files": [ "/home/runner/work/Gatsby_site/Gatsby_site/test2.md", "test.md" ],
   "strings": {
     "good.string": "# good.string\n\nThis string passes all rules.",
     "bad.string": "#bad.string\n\n#This string fails\tsome rules."
