7a1de717b8bf06fd0c786b8d6e9203a0ecada40a
Christina <christinavhastenrath@gmail.com>
2019-10-31 11:19:43 -0700

    stackoverflow

--- mdlint.sh
+++ mdlint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-files=`git fetch origin master:master |
-git diff --name-only master`
+files=`(git fetch origin master:master) && (git diff --name-only master)`
 for x in $files;
 do
 	if [ ${x: -3} == ".md" ]
