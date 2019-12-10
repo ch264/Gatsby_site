8fd167e48fb3c179a4fcd01868c96bda498bff81
Christina <christinavhastenrath@gmail.com>
2019-12-10 19:31:33 +0100

    diffs are now created, stored and send in an email

--- logFile.sh
+++ logFile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 log=log_file.txt
-
+email=email.txt
  
 # create log file or overrite if already present
 printf "Log File - " > $log
@@ -9,11 +9,16 @@ printf "Log File - " > $log
 # append git diff to log file
 git diff master...branch >> $log 
 
-  result=$(grep -n 'name' "$log")
-  if "$result" = true
+result=$(grep -n 'name' && '+' "$log")
+size=${#result}
+ 
+  if [ $size > 0 ]
   then
-		mail -s "H2 changes were found" "christina.hastenrath@getpostman.com" < "$result"
+  # append data to email file
+  printf '%s\n' "${result[@]}" > $email
+  # email the file to an email address
+	mail -s "H2 changes" "christina.hastenrath@getpostman.com" < $email
   else
-    echo no H2 changes
+    echo no H2 diffs between master and dev
 	fi
 
